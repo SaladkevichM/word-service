d0021c71ae198815872d2438fc5c77575dd4da2b
Mikita Saladkevich <Mikita_Saladkevich@epam.com>
2017-11-08 11:26:34 +0300

    Some modifications in sources

--- src/main/java/srv/MySrv.java
+++ src/main/java/srv/MySrv.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MySrv extends  HttpServlet {
+public class MySrv extends HttpServlet {
 
     /**
      * 
--- src/test/java/wordPOI/test/AppTest.java
+++ src/test/java/wordPOI/test/AppTest.java
@@ -1,4 +1,4 @@
-package MyTest.test;
+package wordPOI.test;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
